4a54e554e30c7bae4547714b63a28bb9da8faa85
PedroHenriqueS19 <pedrohenriquef.sousa@outlook.com>
2026-05-23 13:02:47 -0300

    alteracao origins para origin patterns no cors

--- Back/Value-Comparison/src/main/java/com/valuecomparison/security/SecurityConfig.java
+++ Back/Value-Comparison/src/main/java/com/valuecomparison/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://value-comparator.vercel.app"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("https://value-comparator.vercel.app"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
